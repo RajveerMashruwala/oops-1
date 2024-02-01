049ffd010fbb589b34edeae0944461fff7d61aa6
RajveerMashruwala <156444559+RajveerMashruwala@users.noreply.github.com>
2024-02-01 18:04:19 -0500

    comments added

--- burgershop.java
+++ burgershop.java
@@ -9,16 +9,23 @@ public class burgershop {
         String name = scanner.nextLine();
         System.out.println("Enter the number of burgers:");
         int burgers = scanner.nextInt();
-        
+        // calculating total prices of burgers
         double totalPrice = burgers*BURGERPRICE;
+        //calculating total tax 
         double totalTax = totalPrice*0.07;
+        //calculating total amount with taxes
         double totalAmount = totalPrice + totalTax;  
-
+        //printing the customer name
         System.out.println("Customer Name:" + name);
+        //printing the quantity of burgers
         System.out.println("Quantity of the burger:" + burgers);
+        //printing the price of each burger
         System.out.println("Price for the burger" + BURGERPRICE);
+        //printing the total price of all burgers
         System.out.println("Total price:" + totalPrice);
+        //printing the total tax 
         System.out.println("Total Tax:" + totalTax);
+        //printing the total amount
         System.out.println("Total Amount:" + totalAmount);
     }
 }
--- calculator.java
+++ calculator.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args) {
-        int number1;
-        int number2;
+        // intializing the type of variable number 1
+        double number1;
+        //intializing the type of variable number 2
+        double number2;
         Scanner scanner = new Scanner(System.in);
-        Scanner op = new Scanner(System.in);
         System.out.println("Enter the first number:");
         number1 = scanner.nextInt();
         System.out.println("Enter the second number:");
         number2 = scanner.nextInt();
-
+        // to print the sum
         System.out.println("Sum:" + (number1 + number2));
+       // to print the Difference 
         System.out.println("Difference:" + (number1 - number2));
+        // to print the Product
         System.out.println("Product:" + (number1*number2));
+        // to print the Quotent
         System.out.println("Quotent:" + (number1/number2));
+        // to print the Remainder
         System.out.println("Remainder:" + (number1%number2));
     }   
 }
